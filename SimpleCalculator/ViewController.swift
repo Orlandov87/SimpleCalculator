eb12df3beeadf219af5a4846fba14a9b70fed1a2
Orlando Vargas <OrlandoV87@live.com>
2019-06-07 12:46:57 -0400

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Orlando Vargas on 6/7/19.
+//  Copyright © 2019 Orlando Vargas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
